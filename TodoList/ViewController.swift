c195d62b5a20b309ab3eca69190dd12af16adbc4
Mohsen Abdollahi <mohsenabdollahi@Mohsens-Mac.local>
2019-03-15 19:26:09 +0330

    Version 01 TodoList

--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+     var names = [String]()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "\"The List\""
+        tableView.register(UITableViewCell.self,forCellReuseIdentifier: "Cell")
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +27,39 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func addName(_ sender: Any) {
+        // Create AlertView
+        let alert = UIAlertController(title: "New Name",message: "Add a new name",preferredStyle: .alert)
+        
+        // Create Save Button in AlertView
+        let saveAction = UIAlertAction(title: "Save",style: .default,handler: { (action:UIAlertAction) -> Void in
+            let textField = alert.textFields!.first
+            self.names.append(textField!.text!)
+            self.tableView.reloadData()
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel",style: .default) { (action: UIAlertAction) -> Void in
+        }
+        
+        
+        alert.addTextField {(textField: UITextField) -> Void in
+        }
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert,animated: true,completion: nil)
+        
 }
+
+
+func tableView(_ tableView: UITableView,numberOfRowsInSection section: Int) -> Int {
+    return names.count
+}
+func tableView(_ tableView: UITableView,cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+    cell!.textLabel!.text = names[indexPath.row]
+    return cell!
+}
+
+    }
+
 
